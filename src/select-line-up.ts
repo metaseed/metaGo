bb734de8e767f62d0f7415e86ad0224c41f5327d
metasong <metaseed@gmail.com>
2017-04-12 13:58:05 +0800

    selection line up / down

--- src/select-line-up.ts
+++ src/select-line-up.ts
@@ -4,10 +4,37 @@ export class SelectLineUp {
     activate(context: vscode.ExtensionContext) {
         let disposable = vscode.commands.registerCommand('extension.metaGo.selectLineUp', () => {
             const editor = vscode.window.activeTextEditor;
-            const line =editor.selection.active.line;
-//todo
+            const line = editor.selection.active.line - 1;
+            const selection = editor.selection;
+            let anchor = selection.active.line === selection.start.line ? selection.end : selection.start;
+            if (selection.isEmpty)
+                anchor = new vscode.Position(anchor.line, 0);
+            const toLine = line >= 0 ? line : 0;
+            editor.selection = new vscode.Selection(
+                anchor,
+                new vscode.Position(toLine, 0));
+            editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.end));
+
         });
 
         context.subscriptions.push(disposable);
+
+        let disposableD = vscode.commands.registerCommand('extension.metaGo.selectLineDown', () => {
+            const editor = vscode.window.activeTextEditor;
+            const line = editor.selection.active.line + 1;
+            const selection = editor.selection;
+            let anchor = selection.active.line === selection.end.line ? selection.start : selection.end;
+            if (selection.isEmpty)
+                anchor = new vscode.Position(anchor.line, 0);
+            const boundary = editor.document.lineCount;
+            const toLine = line <= boundary ? line : boundary;
+            editor.selection = new vscode.Selection(
+                anchor,
+                new vscode.Position(toLine, 0));
+            editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.end));
+
+        });
+
+        context.subscriptions.push(disposableD);
     }
 }
