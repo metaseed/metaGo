7bf55d694061628ac709cb008847f1f5856fc359
nicchristeller <21079106+nicchristeller@users.noreply.github.com>
2019-11-13 00:10:48 +1300

    Add option to use text-based editor decorations as a workaround for invisible SVG decorations

--- src/config.ts
+++ src/config.ts
@@ -2,73 +2,77 @@ import * as vscode from "vscode";
 import { BookmarkConfig } from './bookmark/config';
 
 export class Config {
-    decoration: DecoratorConfig = new DecoratorConfig();
-    jumper: FinderConfig = new FinderConfig();
-    bookmark = new BookmarkConfig();
+	decoration: DecoratorConfig = new DecoratorConfig();
+	jumper: FinderConfig = new FinderConfig();
+	bookmark = new BookmarkConfig();
 
-    loadConfig = () => {
-        try {
-            this.bookmark.loadConfig();
-            let config = vscode.workspace.getConfiguration("metaGo");
+	loadConfig = () => {
+		try {
+			this.bookmark.loadConfig();
+			let config = vscode.workspace.getConfiguration("metaGo");
 
-            this.decoration.bgColor = config.get<string>("decoration.backgroundColor", this.decoration.bgColor);
-            this.decoration.bgOpacity = config.get<string>("decoration.backgroundOpacity", this.decoration.bgOpacity);
+			this.decoration.useTextBasedDecorations = config.get<boolean>("decoration.useTextBasedDecorations", this.decoration.useTextBasedDecorations);
 
-            this.decoration.color = config.get<string>("decoration.color", this.decoration.color);
-            this.decoration.borderColor = config.get<string>("decoration.borderColor", this.decoration.borderColor);
+			this.decoration.bgColor = config.get<string>("decoration.backgroundColor", this.decoration.bgColor);
+			this.decoration.bgOpacity = config.get<string>("decoration.backgroundOpacity", this.decoration.bgOpacity);
 
-            this.decoration.width = config.get<number>("decoration.width", this.decoration.width);
-            this.decoration.height = config.get<number>("decoration.height", this.decoration.height);
+			this.decoration.color = config.get<string>("decoration.color", this.decoration.color);
+			this.decoration.borderColor = config.get<string>("decoration.borderColor", this.decoration.borderColor);
 
-            this.decoration.x = config.get<number>("decoration.x", this.decoration.x);
-            this.decoration.y = config.get<number>("decoration.y", this.decoration.y);
+			this.decoration.width = config.get<number>("decoration.width", this.decoration.width);
+			this.decoration.height = config.get<number>("decoration.height", this.decoration.height);
 
-            this.decoration.fontSize = config.get<number>("decoration.fontSize", this.decoration.fontSize);
-            this.decoration.fontWeight = config.get<string>("decoration.fontWeight", this.decoration.fontWeight);
-            this.decoration.fontFamily = config.get<string>("decoration.fontFamily", this.decoration.fontFamily);
-            this.decoration.upperCase = config.get<boolean>("decoration.upperCase", this.decoration.upperCase);
-            this.jumper.characters = config.get<string>("decoration.characters", "k, j, d, f, l, s, a, h, g, i, o, n, u, r, v, c, w, e, x, m, b, p, q, t, y, z").split(/[\s,]+/);
+			this.decoration.x = config.get<number>("decoration.x", this.decoration.x);
+			this.decoration.y = config.get<number>("decoration.y", this.decoration.y);
 
-            this.jumper.findAllMode = config.get<string>("jumper.findAllMode", this.jumper.findAllMode);
-            this.jumper.findInSelection = config.get<string>("jumper.findInSelection", this.jumper.findInSelection);
-            this.jumper.wordSeparatorPattern = config.get<string>("jumper.wordSeparatorPattern", this.jumper.wordSeparatorPattern);
-            this.jumper.range = config.get<number>("jumper.screenLineRange", this.jumper.range);
-            this.jumper.targetIgnoreCase = config.get<boolean>("jumper.targetIgnoreCase", this.jumper.targetIgnoreCase);
-            let timeout = config.get<number>("jumper.timeout", this.jumper.timeout);
-            this.jumper.timeout = isNaN(timeout) ? 12000 : timeout * 1000;
-        }
-        catch (e) {
-            vscode.window.showErrorMessage('metaGo: please double check your metaGo config->' + e);
-        }
-    }
+			this.decoration.fontSize = config.get<number>("decoration.fontSize", this.decoration.fontSize);
+			this.decoration.fontWeight = config.get<string>("decoration.fontWeight", this.decoration.fontWeight);
+			this.decoration.fontFamily = config.get<string>("decoration.fontFamily", this.decoration.fontFamily);
+			this.decoration.upperCase = config.get<boolean>("decoration.upperCase", this.decoration.upperCase);
+			this.jumper.characters = config.get<string>("decoration.characters", "k, j, d, f, l, s, a, h, g, i, o, n, u, r, v, c, w, e, x, m, b, p, q, t, y, z").split(/[\s,]+/);
+
+			this.jumper.findAllMode = config.get<string>("jumper.findAllMode", this.jumper.findAllMode);
+			this.jumper.findInSelection = config.get<string>("jumper.findInSelection", this.jumper.findInSelection);
+			this.jumper.wordSeparatorPattern = config.get<string>("jumper.wordSeparatorPattern", this.jumper.wordSeparatorPattern);
+			this.jumper.range = config.get<number>("jumper.screenLineRange", this.jumper.range);
+			this.jumper.targetIgnoreCase = config.get<boolean>("jumper.targetIgnoreCase", this.jumper.targetIgnoreCase);
+			let timeout = config.get<number>("jumper.timeout", this.jumper.timeout);
+			this.jumper.timeout = isNaN(timeout) ? 12000 : timeout * 1000;
+		}
+		catch (e) {
+			vscode.window.showErrorMessage('metaGo: please double check your metaGo config->' + e);
+		}
+	}
 
 }
 
 class DecoratorConfig {
-    bgOpacity: string = '0.88';
-    bgColor: string = "lime,yellow";
-    color: string = "black";
-    borderColor: string = "black";
+	useTextBasedDecorations: boolean = false;
 
-    width: number = 12;
-    height: number = 14;
+	bgOpacity: string = '0.88';
+	bgColor: string = "lime,yellow";
+	color: string = "black";
+	borderColor: string = "black";
 
-    x: number = 2;
-    y: number = 12;
+	width: number = 12;
+	height: number = 14;
 
-    fontSize: number = 14;
-    fontWeight: string = "normal";
-    fontFamily: string = "Consolas";
+	x: number = 2;
+	y: number = 12;
 
-    upperCase: boolean = false;
+	fontSize: number = 14;
+	fontWeight: string = "normal";
+	fontFamily: string = "Consolas";
+
+	upperCase: boolean = false;
 }
 
 class FinderConfig {
-    characters: string[] = ["k", "j", "d", "f", "l", "s", "a", "h", "g", "i", "o", "n", "u", "r", "v", "c", "w", "e", "x", "m", "b", "p", "q", "t", "y", "z"];
-    findAllMode: string = 'on';
-    findInSelection: string = 'off';
-    wordSeparatorPattern: string = "[ ,-.{_(\"'<\\/[+]";
-    range: number = 50;
-    targetIgnoreCase: boolean = true;
-    timeout: number = 12000;
+	characters: string[] = ["k", "j", "d", "f", "l", "s", "a", "h", "g", "i", "o", "n", "u", "r", "v", "c", "w", "e", "x", "m", "b", "p", "q", "t", "y", "z"];
+	findAllMode: string = 'on';
+	findInSelection: string = 'off';
+	wordSeparatorPattern: string = "[ ,-.{_(\"'<\\/[+]";
+	range: number = 50;
+	targetIgnoreCase: boolean = true;
+	timeout: number = 12000;
 }
--- src/metajumper/decoration.ts
+++ src/metajumper/decoration.ts
@@ -3,128 +3,156 @@ import { DecorationModel } from './decoration-model';
 import { Config } from '../config';
 
 export class Decorator {
-    private config: Config;
-    private cache: { [index: string]: vscode.Uri };
-    private decorations: { [index: number]: vscode.TextEditorDecorationType } = {};
-    public charDecorationType;
+	private config: Config;
+	private cache: { [index: string]: vscode.ThemableDecorationAttachmentRenderOptions };
+	private decorations: { [index: number]: vscode.TextEditorDecorationType } = {};
+	public charDecorationType;
 
-    initialize = (config: Config) => {
-        this.config = config;
-        this.updateCache();
-        this.charDecorationType = vscode.window.createTextEditorDecorationType({
-            backgroundColor: 'rgba(0,255,0,0.3)',
-            borderWidth: '2px',
-            borderStyle: 'solid',
-            light: {
-                // this color will be used in light color themes
-                borderColor: 'rgba(0,255,0,0.3)'
-            },
-            dark: {
-                // this color will be used in dark color themes
-                borderColor: 'rgba(0,255,0,0.3)'
-            }
-        });
-    }
+	initialize = (config: Config) => {
+		this.config = config;
+		this.updateCache();
+		this.charDecorationType = vscode.window.createTextEditorDecorationType({
+			backgroundColor: 'rgba(0,255,0,0.3)',
+			borderWidth: '2px',
+			borderStyle: 'solid',
+			light: {
+				// this color will be used in light color themes
+				borderColor: 'rgba(0,255,0,0.3)'
+			},
+			dark: {
+				// this color will be used in dark color themes
+				borderColor: 'rgba(0,255,0,0.3)'
+			}
+		});
+	}
 
-    addCommandIndicator = (editor: vscode.TextEditor) => {
-        let line = editor.selection.anchor.line;
-        let char = editor.selection.anchor.character;
-        let option = [new vscode.Range(line, char, line, char)];
-        editor.setDecorations(this.charDecorationType, option);
-    }
+	addCommandIndicator = (editor: vscode.TextEditor) => {
+		let line = editor.selection.anchor.line;
+		let char = editor.selection.anchor.character;
+		let option = [new vscode.Range(line, char, line, char)];
+		editor.setDecorations(this.charDecorationType, option);
+	}
 
-    removeCommandIndicator = (editor: vscode.TextEditor) => {
-        let locations: vscode.Range[] = [];
-        vscode.window.activeTextEditor.setDecorations(this.charDecorationType, locations);
-    }
+	removeCommandIndicator = (editor: vscode.TextEditor) => {
+		let locations: vscode.Range[] = [];
+		vscode.window.activeTextEditor.setDecorations(this.charDecorationType, locations);
+	}
 
-    addDecorations = (editor: vscode.TextEditor, decorationModel: DecorationModel[]) => {
-        let decorationType = this.createTextEditorDecorationType(1);
-        let decorationType2 = this.createTextEditorDecorationType(2);
+	addDecorations = (editor: vscode.TextEditor, decorationModel: DecorationModel[]) => {
+		let decorationType = this.createTextEditorDecorationType(1);
+		let decorationType2 = this.createTextEditorDecorationType(2);
 
-        let options = [];
-        let options2 = [];
-        decorationModel.forEach((model) => {
-            let code = model.code;
-            let len = code.length;
+		let options = [];
+		let options2 = [];
+		decorationModel.forEach((model) => {
+			let code = model.code;
+			let len = code.length;
 
-            let option: any;
-            if (len === 1) {
-                option = this.createDecorationOptions(null, model.line, model.character + 1, model.character + 1, code);
-                options.push(option);
-            }
-            else {
-                option = this.createDecorationOptions(null, model.line, model.character + 1, model.character + len, code);
-                options2.push(option);
-            }
-        })
-        editor.setDecorations(decorationType, options);
-        editor.setDecorations(decorationType2, options2);
-    }
+			let option: any;
+			if (len === 1) {
+				option = this.createDecorationOptions(null, model.line, model.character + 1, model.character + 1, code);
+				options.push(option);
+			}
+			else {
+				option = this.createDecorationOptions(null, model.line, model.character + 1, model.character + len, code);
+				options2.push(option);
+			}
+		});
 
-    removeDecorations = (editor: vscode.TextEditor) => {
-        for (var dec in this.decorations) {
-            if (this.decorations[dec] === null) continue;
-            editor.setDecorations(this.decorations[dec], []);
-            this.decorations[dec].dispose();
-            this.decorations[dec] = null;
-        }
-    }
+		editor.setDecorations(decorationType, options);
+		editor.setDecorations(decorationType2, options2);
+	}
 
-    private createTextEditorDecorationType = (charsToOffset: number) => {
-        let decorationType = this.decorations[charsToOffset];
-        if (decorationType) return decorationType;
-        decorationType = vscode.window.createTextEditorDecorationType({
-            after: {
-                margin: `0 0 0 ${charsToOffset * (-this.config.decoration.width)}px`,
-                height: `${this.config.decoration.height}px`,
-                width: `${charsToOffset * this.config.decoration.width}px`
-            }
-        });
-        this.decorations[charsToOffset] = decorationType;
-        return decorationType;
-    }
+	removeDecorations = (editor: vscode.TextEditor) => {
+		for (var dec in this.decorations) {
+			if (this.decorations[dec] === null) continue;
+			editor.setDecorations(this.decorations[dec], []);
+			this.decorations[dec].dispose();
+			this.decorations[dec] = null;
+		}
+	}
 
-    private createDecorationOptions = (context: vscode.ExtensionContext, line: number, startCharacter: number, endCharacter: number, code: string): vscode.DecorationOptions => {
-        return {
-            range: new vscode.Range(line, startCharacter, line, endCharacter),
-            renderOptions: {
-                dark: {
-                    after: {
-                        contentIconPath: this.getUri(code)
-                    }
-                },
-                light: {
-                    after: {
-                        contentIconPath: this.getUri(code)
-                    }
-                }
-            }
-        };
-    }
+	private createTextEditorDecorationType = (charsToOffset: number) => {
+		let decorationType = this.decorations[charsToOffset];
+		if (decorationType) return decorationType;
+		decorationType = vscode.window.createTextEditorDecorationType({
+			after: {
+				margin: `0 0 0 ${charsToOffset * (-this.config.decoration.width)}px`,
+				height: `${this.config.decoration.height}px`,
+				width: `${charsToOffset * this.config.decoration.width}px`
+			}
+		});
+		this.decorations[charsToOffset] = decorationType;
+		return decorationType;
+	}
 
-    private getUri = (code: string) => {
-        if (this.cache[code] != undefined)
-            return this.cache[code];
-        this.cache[code] = this.buildUri(code);
-        return this.cache[code];
-    }
+	private createDecorationOptions = (context: vscode.ExtensionContext, line: number, startCharacter: number, endCharacter: number, code: string): vscode.DecorationOptions => {
+		const renderOptions = this.getAfterRenderOptions(code);
+		return {
+			range: new vscode.Range(line, startCharacter, line, endCharacter),
+			renderOptions: {
+				dark: {
+					after: renderOptions,
+				},
+				light: {
+					after: renderOptions,
+				},
+			}
+		};
+	}
 
-    private updateCache = () => {
-        this.cache = {};
-        this.config.jumper.characters
-            .forEach(code => this.cache[code] = this.buildUri(code))
-    }
+	private getAfterRenderOptions = (code: string) => {
+		if (this.cache[code] !== undefined)
+			return this.cache[code];
+		this.cache[code] = this.buildAfterRenderOptions(code);
+		return this.cache[code];
+	}
 
-    private buildUri = (code: string) => {
-        let cf = this.config.decoration;
-        let key = this.config.decoration.upperCase ? code.toUpperCase() : code.toLowerCase();
-        let width = code.length * cf.width;
-        let colors = cf.bgColor.split(',');
-        let bgColor = colors[(code.length - 1) % colors.length];
-        let svg =
-            `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${cf.height}" height="${cf.height}" width="${width}"><rect width="${width}" height="${cf.height}" rx="2" ry="3" style="fill: ${bgColor};fill-opacity:${cf.bgOpacity};stroke:${cf.borderColor};stroke-opacity:${cf.bgOpacity};"/><text font-family="${cf.fontFamily}" font-weight="${cf.fontWeight}" font-size="${cf.fontSize}px" style="fill:${cf.color}" x="${cf.x}" y="${cf.y}">${key}</text></svg>`;
-        return vscode.Uri.parse(svg);
-    }
+	private updateCache = () => {
+		this.cache = {};
+		this.config.jumper.characters
+			.forEach(code => this.cache[code] = this.buildAfterRenderOptions(code))
+	}
+
+	private buildAfterRenderOptions = (code: string) => {
+		return this.config.decoration.useTextBasedDecorations
+			? this.buildAfterRenderOptionsText(code)
+			: this.buildAfterRenderOptionsSvg(code)
+			;
+	}
+
+	private buildAfterRenderOptionsText = (code: string) => {
+		let cf = this.config.decoration;
+		let key = cf.upperCase ? code.toUpperCase() : code.toLowerCase();
+
+		const knownColors = {
+			chartreuse: `rgba(127,255,0,${cf.bgOpacity})`,
+			yellow: `rgba(255,255,0,${cf.bgOpacity})`,
+		};
+
+		let colors = cf.bgColor.split(',');
+		let bgColor = colors[(code.length - 1) % colors.length];
+		bgColor = knownColors[bgColor] || bgColor;
+
+		return {
+			contentText: key,
+			backgroundColor: bgColor,
+			fontWeight: cf.fontWeight,
+			color: cf.color,
+		};
+	}
+
+	private buildAfterRenderOptionsSvg = (code: string) => {
+		let cf = this.config.decoration;
+		let key = this.config.decoration.upperCase ? code.toUpperCase() : code.toLowerCase();
+		let width = code.length * cf.width;
+		let colors = cf.bgColor.split(',');
+		let bgColor = colors[(code.length - 1) % colors.length];
+		let svg =
+			`data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${cf.height}" height="${cf.height}" width="${width}"><rect width="${width}" height="${cf.height}" rx="2" ry="3" style="fill: ${bgColor};fill-opacity:${cf.bgOpacity};stroke:${cf.borderColor};stroke-opacity:${cf.bgOpacity};"/><text font-family="${cf.fontFamily}" font-weight="${cf.fontWeight}" font-size="${cf.fontSize}px" style="fill:${cf.color}" x="${cf.x}" y="${cf.y}">${key}</text></svg>`;
+		return {
+			contentIconPath: vscode.Uri.parse(svg)
+		};
+	}
 
 }
