399de61a001ffed133f8842c8056a1ec92109179
metasong <metaseed@gmail.com>
2020-04-15 10:23:26 -0500

    refactor select-line

--- src/select-lines.ts
+++ src/select-lines.ts
@@ -1,37 +1,38 @@
 import * as vscode from 'vscode';
-import { Utilities } from './lib';
 
 export class SelectLines {
+
     constructor(context: vscode.ExtensionContext) {
         let disposable = vscode.commands.registerCommand('metaGo.selectLineUp', () => {
             const editor = vscode.window.activeTextEditor;
-            const line = editor.selection.active.line - 1;
+            const aboveLine = editor.selection.active.line - 1;
+            const toLine = aboveLine >= 0 ? aboveLine : 0;
             const selection = editor.selection;
             let anchor = selection.anchor;
             if (selection.isEmpty)
                 anchor = new vscode.Position(anchor.line + 1, 0);
-            const toLine = line >= 0 ? line : 0;
-            editor.selection = new vscode.Selection(anchor,new vscode.Position(toLine, 0));
-            editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.end));
 
+            editor.selection = new vscode.Selection(anchor, new vscode.Position(toLine, 0));
+            editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.end));
         });
 
         context.subscriptions.push(disposable);
 
-        let disposableD = vscode.commands.registerCommand('metaGo.selectLineDown', () => {
+        let disposableDown = vscode.commands.registerCommand('metaGo.selectLineDown', () => {
             const editor = vscode.window.activeTextEditor;
-            const line = editor.selection.active.line + 1;
+            const belowLine = editor.selection.active.line + 1;
+            const boundary = editor.document.lineCount;
+            const toLine = belowLine <= boundary ? belowLine : boundary;
             const selection = editor.selection;
             let anchor = selection.anchor;
             if (selection.isEmpty)
                 anchor = new vscode.Position(anchor.line, 0);
-            const boundary = editor.document.lineCount;
-            const toLine = line <= boundary ? line : boundary;
-            editor.selection = new vscode.Selection(anchor,new vscode.Position(toLine, 0));
+                
+            editor.selection = new vscode.Selection(anchor, new vscode.Position(toLine, 0));
             editor.revealRange(new vscode.Range(editor.selection.start, editor.selection.end));
-
         });
 
-        context.subscriptions.push(disposableD);
+        context.subscriptions.push(disposableDown);
     }
+
 }
