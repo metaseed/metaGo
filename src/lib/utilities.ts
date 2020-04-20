e75a9dd655433b54c12bdada7d3d338bfaebe799
metasong <metaseed@gmail.com>
2020-04-20 13:48:41 -0500

    metajump add cursor to change active selection

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -4,16 +4,24 @@ export class Utilities {
     public static goto(editor: vscode.TextEditor, line: number = -1, character: number = -1, addCursor = false) {
         line = line === -1 ? editor.selection.active.line : line;
         character = character === -1 ? editor.selection.active.character : character;
+
+        for (const selection of editor.selections) {
+            if (selection.contains(new vscode.Position(line, character))) {
+                const selections = editor.selections.filter(s=>s !== selection);
+                editor.selections = [...selections, selection];
+                return;
+            }
+        }
         this.select(editor, line, character, line, character, addCursor);
     }
 
-    public static select(editor: vscode.TextEditor, fromLine: number, fromCharacter: number, toLine: number, toCharacter: number, addCursor =false) {
+    public static select(editor: vscode.TextEditor, fromLine: number, fromCharacter: number, toLine: number, toCharacter: number, addCursor = false) {
         const startRange = new vscode.Position(fromLine, fromCharacter);
         const endRange = new vscode.Position(toLine, toCharacter);
-        if(addCursor){
+        if (addCursor) {
             editor.selections = [...editor.selections, new vscode.Selection(startRange, endRange)];
         } else {
-            const selections = editor.selections.slice(0, editor.selections.length -1);
+            const selections = editor.selections.slice(0, editor.selections.length - 1);
             const selection = new vscode.Selection(startRange, endRange);
             editor.selections = [...selections, selection];
         }
