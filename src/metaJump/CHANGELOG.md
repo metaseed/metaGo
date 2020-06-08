a302a1e1e008a0859b78309120c76c95889319ac
metasong <metaseed@gmail.com>
2020-06-08 16:16:20 -0500

    metaJump

---
+++ src/metaJump/CHANGELOG.md
@@ -0,0 +1,21 @@
+# Change Log
+All notable changes to the "metaJump" extension will be documented in this file.
+
+## Todo List:
+## In Development
+---
+### 
+#### Added
+#### Removed
+#### Changed
+#### Fixed
+
+## Current Version
+---
+### V1.0.0 June 08, 2020
+#### Added
+#### Removed
+#### Changed
+ - [x] MetaJump extracted from MetaGo as a separated vscode extension.
+ - [x] rename insert* command to Add* command.
+#### Fixed
---
+++ src/metaJump/webpack.config.js
@@ -0,0 +1,74 @@
+'use strict';
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+function resolveTsconfigPathsToAlias({
+  tsconfigPath = './tsconfig.json',
+  webpackConfigBasePath = __dirname,
+} = {}) {
+  const { paths } = require(tsconfigPath).compilerOptions;
+
+  const aliases = {};
+
+  Object.keys(paths).forEach((item) => {
+    const key = item.replace('/*', '');
+    const value = path.resolve(webpackConfigBasePath, paths[item][0].replace('/*', '').replace('*', ''));
+
+    aliases[key] = value;
+  });
+
+  return aliases;
+}
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      terserOptions: {
+        ecma: 8,
+        keep_classnames: false,
+        mangle: true,
+        module: true
+      }
+    })],
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js'],
+    alias: resolveTsconfigPathsToAlias(),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$|\.css$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
